b78f79b7bdd1a84b3ba3178308a2f24f9a6bc521
Dimacy <dimacy95@mail.ru>
2020-04-13 15:43:45 +0300

    xxx

--- App.js
+++ App.js
@@ -12,11 +12,7 @@ import {
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-<<<<<<< HEAD
 import Income from './src/Components/IncomeCategory';
-=======
-import Profile from './src/Components/ProfileCategory';
->>>>>>> master
 import Costs from './src/Components/CostsCategory';
 
 const App = () => {
