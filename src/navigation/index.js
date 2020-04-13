1f48a30844e104101cac9ad8c07d717ba961b887
Dimacy <dimacy95@mail.ru>
2020-04-13 16:15:05 +0300

    remove createContext from navigation/index.js

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -5,19 +5,8 @@ import CategoriesNav from '../navigation/CatigoriesNavigation';
 import NewCategory from '../screens/NewCategory';
 
 const Stack = createStackNavigator();
-export const CategoryContext = createContext();
 
 function Navigator() {
-  const [incomeCategory, setIncomeCategory] = useState([
-    {iconName: 'credit-card', name: 'Кошелек'},
-    {iconName: 'bank', name: 'Банковские вложения'},
-  ]);
-  const [costsCategory, setCostsCategory] = useState([
-    {iconName: 'home', name: 'Дом'},
-    {iconName: 'car', name: 'Машина'},
-    {iconName: 'airplane', name: 'Отдых'},
-    {iconName: 'heart', name: 'Здоровье'},
-  ]);
   return (
     <Stack.Navigator>
       <Stack.Screen
